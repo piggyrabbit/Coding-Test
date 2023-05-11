e2dd2b895ce776d09566cef49296a524ac1965a9
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-11 10:24:42 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
