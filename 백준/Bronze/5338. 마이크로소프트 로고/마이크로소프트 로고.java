9eb8601f7dffffa913cf12042da683cc96fa3f71
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-12 12:29:13 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}
