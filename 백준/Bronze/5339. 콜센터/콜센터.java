974679eedeaecaaf6ac0707d2a06927001b70771
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-12 12:29:23 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |");
+	}
+}
