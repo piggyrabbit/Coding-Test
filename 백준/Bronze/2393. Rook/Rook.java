52f3b19a210469e3ff85e471ab7f623b314ee1b4
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-18 13:12:29 +0900

    [Bronze V] Title: Rook, Time: 116 ms, Memory: 13884 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("  ___  ___  ___\n"
+				+ "  | |__| |__| |\n"
+				+ "  |           |\n"
+				+ "   \\_________/\n"
+				+ "    \\_______/\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |     |\n"
+				+ "     |_____|\n"
+				+ "  __/       \\__\n"
+				+ " /             \\\n"
+				+ "/_______________\\");
+		
+	}
+}
