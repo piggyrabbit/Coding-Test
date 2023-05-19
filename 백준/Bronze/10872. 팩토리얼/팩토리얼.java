b9d545e5368e8a8b2988739b0757842bcc95b90c
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-19 12:38:01 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int result = 1;
+		
+		for (int i = 1; i <=n; i++) {
+			result *= i;
+		}
+		System.out.println(result);
+	}
+}
