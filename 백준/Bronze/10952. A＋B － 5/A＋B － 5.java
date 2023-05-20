b3ada94355e27974f3fe927993e451133fb41e23
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-20 23:10:17 +0900

    [Bronze V] Title: A+B - 5, Time: 228 ms, Memory: 17908 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		while(true) {
+			a = scan.nextInt();
+			b = scan.nextInt();
+			if(a == 0 && b == 0) break;
+			else System.out.println(a+b);
+		}
+	}
+
+}
