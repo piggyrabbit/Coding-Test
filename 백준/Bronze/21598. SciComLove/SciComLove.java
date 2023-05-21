4d5590753e993c57493be5ff3699b328d687ea95
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-21 12:20:41 +0900

    [Bronze V] Title: SciComLove, Time: 204 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println("SciComLove");
+		}
+		
+	}
+}
