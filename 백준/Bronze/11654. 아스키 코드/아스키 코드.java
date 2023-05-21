b096c72f7660dabca212f8273bdbf5c3dd2a23fa
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-21 12:21:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 148 ms, Memory: 15896 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		char ch = str.charAt(0);
+		
+		bw.write((int)ch + "\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
