4a69f74d48d926cd4da55f61a027246f6c41ea7b
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-22 09:49:35 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 228 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		System.out.println(1);
+		System.out.println(0);
+		
+	}
+}
