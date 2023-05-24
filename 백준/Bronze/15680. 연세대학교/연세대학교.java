aebf8a49eb6f21b59a5a9ad325b4f85c30717b0d
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-24 10:50:35 +0900

    [Bronze V] Title: 연세대학교, Time: 200 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		if (n == 0) System.out.println("YONSEI");
+		if (n == 1) System.out.println("Leading the Way to the Future");
+	}
+
+}
