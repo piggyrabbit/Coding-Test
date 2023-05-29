5d8738d0475b7bd9b4da00ef5555572cff937afc
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-05-29 13:07:49 +0900

    [Bronze V] Title: 문자와 문자열, Time: 148 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str;
+		
+		str = br.readLine();
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		char[] ch = str.toCharArray();
+		
+		bw.write(ch[num - 1] + "\n");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
