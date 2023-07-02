29841b637da1c88fc9b961af2df4c7581a4d299d
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-02 20:39:42 +0900

    [Bronze V] Title: 이상한 기호, Time: 200 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -7,9 +7,13 @@ public class Main {
 
 		Scanner scan = new Scanner(System.in);
 		
-		int a = scan.nextInt();
-		int b = scan.nextInt();
+		String a = scan.next();
+		String b = scan.next();
+		
+		BigInteger A = new BigInteger(a);
+		BigInteger B = new BigInteger(b);
+		
+		System.out.println((A.add(B)).multiply(A.subtract(B)));
 
-		System.out.println((a+b)*(a-b));
 	}
 }
