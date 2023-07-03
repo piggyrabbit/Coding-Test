594b7596ba7f9848961003470be2702d98cac72c
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-03 12:45:47 +0900

    [level 1] Title: 폰켓몬, Time: 2.50 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        return answer;
+        
+        int num = nums.length / 2;
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        if (num > set.size()) {
+            num = set.size();
+        }
+        
+        return num;
     }
 }
