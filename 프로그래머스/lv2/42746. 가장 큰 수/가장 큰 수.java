93e9b3aaa4abd48c6c2120f6110924ad556b0d0c
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-11 13:08:09 +0900

    [level 2] Title: 가장 큰 수, Time: 1.67 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,33 +1,51 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // 숫자 배열을 문자열 배열로 변환
+        
+        String answer = "";
+        
         String[] arr = new String[numbers.length];
+        
         for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+            arr[i] = numbers[i] + "";
         }
-
-        // 정렬
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                // ab와 ba를 비교하여 큰 순서대로 정렬
-                return (b + a).compareTo(a + b);
+        
+        Arrays.sort(arr);
+        
+        for (int i = arr.length - 1; i > 0; i--) {
+            // 같은 앞자리수의 String을 찾아서 가장 적합한 것 찾기
+            // i 인덱스에 해당하는 값과 j 인덱스 값 교환
+            for (int j = i - 1; j >= 0; j--) {
+                // 첫 자리가 같을 경우
+                if (arr[i].substring(0,1).equals(arr[j].substring(0,1))) {
+                    // 다른 자리를 비교
+                    
+                    if (Integer.parseInt(arr[i] + arr[j]) < Integer.parseInt(arr[j] + arr[i])) {
+                        String temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                    
+                    
+                    // 더 적합한 것을 찾을 경우 위치 변경
+                }
             }
-        });
-
-        // 0으로만 구성된 배열인 경우 예외 처리
-        if (arr[0].equals("0")) {
-            return "0";
         }
-
-        // 정렬된 문자열을 이어붙여 결과 반환
-        StringBuilder sb = new StringBuilder();
-        for (String str : arr) {
-            sb.append(str);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            answer += arr[i];
         }
-        return sb.toString();
+        
+        int count = 0;
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i].equals("0")) count++;
+        }
+        
+        if (count == arr.length) answer = "0";
+        
+        System.out.println(answer);
+        
+        return answer;
     }
 }
