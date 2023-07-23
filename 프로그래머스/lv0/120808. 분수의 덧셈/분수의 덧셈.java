1fcc1eb7c53e06de767b8043185560c27726b14c
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-23 16:53:42 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.04 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+        
+        answer[0] = numer1 * denom2 + numer2 * denom1;
+        answer[1] = denom1 * denom2;
+        
+        int max = 1;
+        
+        for (int i = 2; i < 1000000; i++) {
+            if (answer[0]%i == 0 && answer[1]%i == 0) {
+                max = i;
+            }
+        }
+        
+        answer[0] = answer[0] / max;
+        answer[1] = answer[1] / max;
+        
+        return answer;
+    }
+}
