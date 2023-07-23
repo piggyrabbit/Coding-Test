b2c0cea865b20c487ee6629d6af859da64dc776e
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-23 16:43:38 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        if ((double)n/slice > n/slice) answer = n/slice + 1;
+        else answer = n/slice;
+        
+        return answer;
+    }
+}
