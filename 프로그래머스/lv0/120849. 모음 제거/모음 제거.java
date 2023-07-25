b0e8d6fb0ad2952bf955e88a898b7f15cc1019c8
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-25 15:28:18 +0900

    [level 0] Title: 모음 제거, Time: 0.20 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string;
+        
+        answer = answer.replaceAll("a", "");
+        answer = answer.replaceAll("e", "");
+        answer = answer.replaceAll("i", "");
+        answer = answer.replaceAll("o", "");
+        answer = answer.replaceAll("u", "");
+
+        
+        return answer;
+    }
+}
