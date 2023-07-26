7c125547c01f8d7fa8687ac1868236cad3b2c3ef
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-26 11:15:49 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 12.23 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        char[] arr = my_string.toCharArray();
+        
+        char tmp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = tmp;
+        
+        for (char ch : arr) {
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
