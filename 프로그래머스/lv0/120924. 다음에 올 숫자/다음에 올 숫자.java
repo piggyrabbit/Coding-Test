72cff33c53b6e7e22c49546c072e4545515e3e26
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-07-29 23:55:30 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        // 등차수열
+        if (common[1] - common[0] == common[2] - common[1]) {
+            answer = common[common.length - 1] + (common[2] - common[1]);
+        }
+        // 등비수열
+        else {
+            answer = common[common.length - 1] * (common[2]/common[1]);
+        }
+        return answer;
+    }
+}
