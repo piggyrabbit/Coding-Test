9a28ae644363fbcd7ff42bba8d81de5274c23e1c
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-08-01 09:48:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.47 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -4,41 +4,36 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
         
-        HashMap<Integer, Integer> set = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < array.length; i++) {
-            if (set.containsKey(array[i])) {
-                set.put(array[i], set.get(array[i]) + 1);
+            if (map.get(array[i]) == null) {
+                map.put(array[i], 1);
+            } else {
+                map.put(array[i], map.get(array[i]) + 1);
             }
-            else {
-                set.put(array[i], 1);
+        }
+        
+        int max = 0;
+        
+        for (int value : map.values()) {
+            if (max < value) max = value;
+        }
+        
+        System.out.println(max);
+        
+        for (int key : map.keySet()) {
+            if (map.get(key) == max && answer == 0) {
+                answer = key;
+            }
+            else if (map.get(key) == max && answer != 0) {
+                answer = -1;
             }
         }
         
-        int max = 1;
-        
-        for (int value : set.values()) {
-            if (value > max) max = value;
-        }
-        
-        int num = 0;
-        
-        int cnt = 0;
-        
-        for (int value : set.values()) {
-            if (value == max) num++;
-            if (num == 0) cnt++;
-        }
-        
-        int cnt2 = 0;
-        
-        for (int key : set.keySet()) {
-            cnt2++;
-            if (cnt + 1 == cnt2) answer = key;
-        }
-        
-        if (num > 1) answer = -1;
         
         return answer;
     }
 }
+
+
