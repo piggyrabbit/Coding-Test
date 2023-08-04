1bc6999a08302f505ec40e99c54349aac3dbaeec
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-08-04 10:15:10 +0900

    [unrated] Title: ad 제거하기, Time: 0.93 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] strArr) {
+        
+        ArrayList<String> list = new ArrayList<>();
+        
+        for (String str : strArr) {
+            if (!str.contains("ad")) list.add(str);
+        }
+        
+        return list;
+    }
+}
