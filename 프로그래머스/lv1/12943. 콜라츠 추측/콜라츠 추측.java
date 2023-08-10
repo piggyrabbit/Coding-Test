55bf037122e46463c5ac0f8ba5e12855fc8578e3
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-08-10 15:50:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int answer = -1;
+        
+        for (int i = 0; i < 500; i++) {
+            
+            if (num == 1) {
+                answer = i;
+                break;
+            }
+            
+            if (num % 2 == 0) {
+                num = num / 2;
+            }
+            else if (num % 2 == 1 && num != 1) num = num * 3 + 1;
+        }
+        
+        return answer;
+    }
+}
