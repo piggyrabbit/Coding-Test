923972bd4ad97f0fe01f32755ba0019b8145c274
LEE_SAEYOUNG <57945126+piggyrabbit@users.noreply.github.com>
2023-11-24 09:17:12 +0900

    [Gold V] Title: Contact, Time: 284 ms, Memory: 19112 KB -BaekjoonHub

--- 백준/Gold/1013. Contact/Contact.java
+++ 백준/Gold/1013. Contact/Contact.java
@@ -1,17 +1,34 @@
+import java.io.*;
 import java.util.*;
- 
+
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        String vega = "(100+1+|01)+";
-        int t = scan.nextInt();
-        
-        for(int i = 0; i < t; i++) {
-            String spread = scan.next();
-            if(spread.matches(vega)) System.out.println("YES");
-            else System.out.println("NO");
+
+
+    static int N;
+    static String no = "NO";
+    static String yes = "YES";
+
+    //패턴 : (100+1+ | 01) + -> 정규식 사용!!!!
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<N; i++){
+            if(parser(br.readLine())){
+                System.out.println(yes);
+            }else{
+                System.out.println(no);
+            }
         }
+
     }
-}   
+
+    public static boolean parser(String s){
+        String pattern = "^(01|10(0+1+))+$";
+        boolean val = s.matches(pattern);
+        return val;
+    }
+}
